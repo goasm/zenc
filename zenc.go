10d3597040a9c5a89ef3abfbd3a438256dd50a8d
skylerlee <skyler.ac.lee@gmail.com>
2019-02-17 17:32:16 +0800

    feat: use pbkdf2 for keygen

--- zenc.go
+++ zenc.go
@@ -1,19 +1,29 @@
 package zenc
 
 import (
+	"crypto/sha256"
 	"os"
+
+	"golang.org/x/crypto/pbkdf2"
 )
+
+func keygen(pass string, len int) []byte {
+	salt := []byte("zenc-v1.0")
+	return pbkdf2.Key([]byte(pass), salt, 4096, len, sha256.New)
+}
 
 // EncryptFile encrypts file using the given password
 func EncryptFile(ifile, ofile *os.File, pass string) {
+	iv := make([]byte, 16)
 	pipeline := NewPipeline()
-	pipeline.AddStage(NewEncrypter())
+	pipeline.AddStage(NewEncrypter(keygen(pass, 32), iv))
 	pipeline.Run(ifile, ofile)
 }
 
 // DecryptFile decrypts file using the given password
 func DecryptFile(ifile, ofile *os.File, pass string) {
+	iv := make([]byte, 16)
 	pipeline := NewPipeline()
-	pipeline.AddStage(NewDecrypter())
+	pipeline.AddStage(NewDecrypter(keygen(pass, 32), iv))
 	pipeline.Run(ifile, ofile)
 }
