1ef183193b1dd86f142bda0c011f0599321a3236
skylerlee <skyler.ac.lee@gmail.com>
2019-03-06 00:55:56 +0800

    fix: fix filehead data copy bug

--- filehead.go
+++ filehead.go
@@ -14,8 +14,8 @@ type FileHead struct {
 }
 
 // NewFileHead creates a new FileHead
-func NewFileHead() FileHead {
-	head := FileHead{
+func NewFileHead() *FileHead {
+	head := &FileHead{
 		Magic:    0x5A454E43, // ZENC
 		Checksum: 0,
 	}
@@ -27,16 +27,16 @@ func NewFileHead() FileHead {
 }
 
 // ReadDataFrom reads the data of FileHead from r
-func (h FileHead) ReadDataFrom(r io.Reader) {
-	err := binary.Read(r, binary.BigEndian, &h)
+func (h *FileHead) ReadDataFrom(r io.Reader) {
+	err := binary.Read(r, binary.BigEndian, h)
 	if err != nil {
 		panic(err)
 	}
 }
 
 // WriteDataTo writes the data of FileHead to w
-func (h FileHead) WriteDataTo(w io.Writer) {
-	err := binary.Write(w, binary.BigEndian, &h)
+func (h *FileHead) WriteDataTo(w io.Writer) {
+	err := binary.Write(w, binary.BigEndian, h)
 	if err != nil {
 		panic(err)
 	}
