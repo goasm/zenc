d2860fa4e5915bd5f1b604604f155dfc26d5d639
skylerlee <skyler.ac.lee@gmail.com>
2019-03-09 19:58:20 +0800

    feat: update checker

--- checker.go
+++ checker.go
@@ -1,21 +1,20 @@
 package zenc
 
-import "hash/crc32"
-
-const polynomial uint32 = 0xEDB88320
+import (
+	"hash/crc32"
+)
 
 type checker struct {
 	sum uint32
-	tab *crc32.Table
 }
 
 // NewChecker creates a Stage for calculating checksum
 func NewChecker() Stage {
-	return &checker{0, crc32.MakeTable(polynomial)}
+	return &checker{0}
 }
 
 func (c *checker) Process(data []byte) []byte {
-	c.sum = crc32.Update(c.sum, c.tab, data)
+	c.sum = crc32.Update(c.sum, crc32.IEEETable, data)
 	return data
 }
 
