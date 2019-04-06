25b5144870b59a1f7a33fd110f3601aa43dfb69c
skylerlee <skyler.ac.lee@gmail.com>
2019-04-06 22:13:19 +0800

    feat: update chunk

--- chunk.go
+++ chunk.go
@@ -4,24 +4,34 @@ import (
 	"io"
 )
 
+const (
+	maxChunkSize = 4096
+)
+
 type chunkReader struct {
+	r io.Reader
 }
 
+// NewChunkReader creates a Reader that reads chunks into continuous bytes
 func NewChunkReader(r io.Reader) io.Reader {
-	return &chunkReader{}
+	return &chunkReader{r}
 }
 
-func (c *chunkReader) Read(dest []byte) (n int, err error) {
+func (c *chunkReader) Read(dst []byte) (n int, err error) {
+	n, err = c.r.Read(dst)
 	return
 }
 
 type chunkWriter struct {
+	w io.Writer
 }
 
+// NewChunkWriter creates a Writer that writes continuous bytes to chunks
 func NewChunkWriter(w io.Writer) io.Writer {
-	return &chunkWriter{}
+	return &chunkWriter{w}
 }
 
 func (c *chunkWriter) Write(src []byte) (n int, err error) {
+	n, err = c.w.Write(src)
 	return
 }
