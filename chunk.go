5765db658e30b71169f1a38f6d038411a4218cdd
skylerlee <skyler.ac.lee@gmail.com>
2019-04-06 22:43:15 +0800

    feat: implement chunk writer

--- chunk.go
+++ chunk.go
@@ -1,6 +1,8 @@
 package zenc
 
 import (
+	"bytes"
+	"encoding/binary"
 	"io"
 )
 
@@ -23,15 +25,38 @@ func (c *chunkReader) Read(dst []byte) (n int, err error) {
 }
 
 type chunkWriter struct {
+	b bytes.Buffer
 	w io.Writer
 }
 
 // NewChunkWriter creates a Writer that writes continuous bytes to chunks
 func NewChunkWriter(w io.Writer) io.Writer {
-	return &chunkWriter{w}
+	return &chunkWriter{bytes.Buffer{}, w}
 }
 
 func (c *chunkWriter) Write(src []byte) (n int, err error) {
-	n, err = c.w.Write(src)
+	for c.b.Len() < maxChunkSize {
+		_, err = c.b.Write(src)
+		if err != nil {
+			break
+		}
+	}
+	nw := 0
+	prefix := [4]byte{}
+	chunk := []byte{}
+	for c.b.Len() > 0 {
+		chunk = c.b.Next(maxChunkSize)
+		binary.LittleEndian.PutUint32(prefix[:], uint32(len(chunk)))
+		nw, err = c.w.Write(prefix[:])
+		n += nw
+		if err != nil {
+			break
+		}
+		nw, err = c.w.Write(chunk)
+		n += nw
+		if err != nil {
+			break
+		}
+	}
 	return
 }
