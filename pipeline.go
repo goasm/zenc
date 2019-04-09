7b9fc6c341c4f283f49547d298d3f4871e5d75b9
skylerlee <skyler.ac.lee@gmail.com>
2019-04-10 00:40:19 +0800

    docs: add comments

--- pipeline.go
+++ pipeline.go
@@ -2,24 +2,29 @@ package zenc
 
 import "io"
 
+// Stage represents a data processing step of a Pipeline
 type Stage interface {
 	io.Writer
 	SetNext(Stage)
 	Next() Stage
 }
 
+// Pipeline represents a data flow consisting of one or more Stages
 type Pipeline struct {
 	next Stage
 }
 
+// NewPipeline creates a Pipeline
 func NewPipeline() *Pipeline {
 	return &Pipeline{}
 }
 
+// SetNext sets the next Stage
 func (p *Pipeline) SetNext(n Stage) {
 	p.next = n
 }
 
+// Next gets the next Stage
 func (p *Pipeline) Next() Stage {
 	return p.next
 }
