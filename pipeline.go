d672c9b7e97c249909d71b1e8f0e044f6b962494
skylerlee <skyler.ac.lee@gmail.com>
2019-04-10 00:52:08 +0800

    feat: update pipeline

--- pipeline.go
+++ pipeline.go
@@ -11,14 +11,25 @@ type Stage interface {
 
 // Pipeline represents a data flow consisting of one or more Stages
 type Pipeline struct {
-	next Stage
+	head Stage
+	tail Stage
 }
 
 // NewPipeline creates a Pipeline
 func NewPipeline() *Pipeline {
-	return &Pipeline{}
+	return &Pipeline{nil, nil}
+}
+
+// AddStage adds a Stage at the end of the Pipeline
+func (p *Pipeline) AddStage(stage Stage) {
+	if p.head == nil {
+		p.head = stage
+		p.tail = p.head
+	} else {
+		p.tail.SetNext(stage)
+	}
 }
 
 func (p *Pipeline) Write(data []byte) (int, error) {
-	return p.next.Write(data)
+	return p.head.Write(data)
 }
