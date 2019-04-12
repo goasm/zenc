dc3488cc0ae2aeacb204ec6098680360ead6de0a
skylerlee <skyler.ac.lee@gmail.com>
2019-04-13 01:44:26 +0800

    feat: add checksum stage

---
+++ checksum.go
@@ -0,0 +1,19 @@
+package zenc
+
+import "hash/crc32"
+
+// ChecksumStage maintains a checksum of the data being processed
+type ChecksumStage struct {
+	MiddleStage
+	Sum uint32
+}
+
+// NewChecksumStage creates a Stage for calculating checksum
+func NewChecksumStage() *ChecksumStage {
+	return &ChecksumStage{MiddleStage{}, 0}
+}
+
+func (cs *ChecksumStage) Write(data []byte) (int, error) {
+	cs.Sum = crc32.Update(cs.Sum, crc32.IEEETable, data)
+	return cs.next.Write(data)
+}
