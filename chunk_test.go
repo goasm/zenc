2936d180b62a1be8c3fccb421305356b0a9b29e6
skylerlee <skyler.ac.lee@gmail.com>
2019-04-15 23:25:50 +0800

    test: add test

--- chunk_test.go
+++ chunk_test.go
@@ -37,6 +37,36 @@ func TestChunkSimple(t *testing.T) {
 	}
 }
 
+func TestChunkLarge(t *testing.T) {
+	src := make([]byte, 5000)
+	fillSequence(src)
+	r := bytes.NewReader(src)
+	w := new(bytes.Buffer)
+	cs := zenc.NewChunkStage()
+	cs.SetNext(zenc.NewDestStage(w))
+	io.Copy(cs, r)
+	cs.Flush()
+	if w.Len() != 5008 {
+		t.Fatal("ChunkStage writes wrong length of bytes", w.Len())
+	}
+	size1 := int(binary.LittleEndian.Uint32(w.Next(4)))
+	if size1 != 4096 {
+		t.Fatal("ChunkStage writes wrong chunk size", size1)
+	}
+	chunk1 := w.Next(4096)
+	if chunk1[0] != 0 || chunk1[4095] != 255 {
+		t.Fatal("ChunkStage writes wrong chunk data")
+	}
+	size2 := int(binary.LittleEndian.Uint32(w.Next(4)))
+	if size2 != 904 {
+		t.Fatal("ChunkStage writes wrong chunk size", size2)
+	}
+	chunk2 := w.Next(904)
+	if chunk2[0] != 0 || chunk2[903] != 135 {
+		t.Fatal("ChunkStage writes wrong chunk data")
+	}
+}
+
 func TestUnchunkSimple(t *testing.T) {
 	prefix := [4]byte{}
 	src := make([]byte, 100)
