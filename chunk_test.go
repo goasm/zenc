c188b4d47e981911869bd005cbacfae354a1fdf2
skylerlee <skyler.ac.lee@gmail.com>
2019-04-16 00:13:45 +0800

    refactor: reduce testing code

--- chunk_test.go
+++ chunk_test.go
@@ -9,59 +9,69 @@ import (
 	"github.com/radonlab/zenc"
 )
 
+const (
+	chunkHeadSize int = 4
+)
+
+var (
+	sample []byte
+)
+
+func init() {
+	sample = make([]byte, 10000)
+	fillSequence(sample)
+}
+
 func fillSequence(buf []byte) {
 	for i := 0; i < len(buf); i++ {
 		buf[i] = byte(i)
 	}
 }
 
-func TestChunkSimple(t *testing.T) {
-	src := make([]byte, 100)
-	fillSequence(src)
-	r := bytes.NewReader(src)
-	w := new(bytes.Buffer)
+func getTestChunk(limit int) *bytes.Buffer {
+	data := new(bytes.Buffer)
 	cs := zenc.NewChunkStage()
-	cs.SetNext(zenc.NewDestStage(w))
-	io.Copy(cs, r)
+	cs.SetNext(zenc.NewDestStage(data))
+	io.Copy(cs, io.LimitReader(bytes.NewReader(sample), int64(limit)))
 	cs.Flush()
-	if w.Len() != 104 {
-		t.Fatal("ChunkStage writes wrong length of bytes", w.Len())
+	return data
+}
+
+func TestChunkSimple(t *testing.T) {
+	total := 100
+	out := getTestChunk(total)
+	if out.Len() != total+chunkHeadSize {
+		t.Fatal("ChunkStage writes wrong length of bytes", out.Len())
 	}
-	size := int(binary.LittleEndian.Uint32(w.Next(4)))
-	if size != 100 {
-		t.Fatal("ChunkStage writes wrong chunk size", size)
+	size1 := int(binary.LittleEndian.Uint32(out.Next(chunkHeadSize)))
+	if size1 != 100 {
+		t.Fatal("ChunkStage writes wrong chunk size", size1)
 	}
-	chunk := w.Next(100)
-	if chunk[0] != 0 || chunk[99] != 99 {
+	chunk1 := out.Next(100)
+	if chunk1[0] != 0 || chunk1[99] != 99 {
 		t.Fatal("ChunkStage writes wrong chunk data")
 	}
 }
 
 func TestChunkLarge(t *testing.T) {
-	src := make([]byte, 5000)
-	fillSequence(src)
-	r := bytes.NewReader(src)
-	w := new(bytes.Buffer)
-	cs := zenc.NewChunkStage()
-	cs.SetNext(zenc.NewDestStage(w))
-	io.Copy(cs, r)
-	cs.Flush()
-	if w.Len() != 5008 {
-		t.Fatal("ChunkStage writes wrong length of bytes", w.Len())
+	total := 5000
+	out := getTestChunk(total)
+	if out.Len() != total+chunkHeadSize*2 {
+		t.Fatal("ChunkStage writes wrong length of bytes", out.Len())
 	}
-	size1 := int(binary.LittleEndian.Uint32(w.Next(4)))
+	size1 := int(binary.LittleEndian.Uint32(out.Next(chunkHeadSize)))
 	if size1 != 4096 {
 		t.Fatal("ChunkStage writes wrong chunk size", size1)
 	}
-	chunk1 := w.Next(4096)
+	chunk1 := out.Next(4096)
 	if chunk1[0] != 0 || chunk1[4095] != 255 {
 		t.Fatal("ChunkStage writes wrong chunk data")
 	}
-	size2 := int(binary.LittleEndian.Uint32(w.Next(4)))
+	size2 := int(binary.LittleEndian.Uint32(out.Next(chunkHeadSize)))
 	if size2 != 904 {
 		t.Fatal("ChunkStage writes wrong chunk size", size2)
 	}
-	chunk2 := w.Next(904)
+	chunk2 := out.Next(904)
 	if chunk2[0] != 0 || chunk2[903] != 135 {
 		t.Fatal("ChunkStage writes wrong chunk data")
 	}
