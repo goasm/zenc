12cd8f4d50519ab4f51414d0d0bfde78910ea457
skylerlee <skyler.ac.lee@gmail.com>
2019-04-17 01:25:56 +0800

    test: add test case

--- chunk_test.go
+++ chunk_test.go
@@ -85,6 +85,34 @@ func TestChunkLarge(t *testing.T) {
 	}
 }
 
+func TestChunkFull(t *testing.T) {
+	total := 8192
+	out := getSampleChunkData(total)
+	if out.Len() != total+chunkHeadSize*3 {
+		t.Fatal("ChunkStage writes a wrong total length", out.Len())
+	}
+	size1 := int(binary.LittleEndian.Uint32(out.Next(chunkHeadSize)))
+	if size1 != 4096 {
+		t.Fatal("ChunkStage writes a wrong chunk length", size1)
+	}
+	chunk1 := out.Next(4096)
+	if !bytes.Equal(chunk1, sample[:4096]) {
+		t.Fatal("ChunkStage writes mismatched chunk data")
+	}
+	size2 := int(binary.LittleEndian.Uint32(out.Next(chunkHeadSize)))
+	if size2 != 4096 {
+		t.Fatal("ChunkStage writes a wrong chunk length", size2)
+	}
+	chunk2 := out.Next(4096)
+	if !bytes.Equal(chunk2, sample[4096:8192]) {
+		t.Fatal("ChunkStage writes mismatched chunk data")
+	}
+	size3 := int(binary.LittleEndian.Uint32(out.Next(chunkHeadSize)))
+	if size3 != 0 {
+		t.Fatal("ChunkStage writes a wrong chunk length", size3)
+	}
+}
+
 func TestUnchunkSimple(t *testing.T) {
 	prefix := [4]byte{}
 	src := make([]byte, 100)
